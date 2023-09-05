0b8add1a04592afb9d255d055f84f4d6d0e501e6
kaanygit <yasinkaan345@hotmail.com>
2023-09-05 18:43:59 +0300

    Fixed bug when fetch data

--- src/app/api/datacom/posts/route.ts
+++ src/app/api/datacom/posts/route.ts
@@ -1,17 +1,18 @@
-import CommunityData from '@/lib/models/comdata';
+import CommunityDataSchema from '@/lib/models/post-model';
 import startDatabase from '@/lib/db';
 import { NextResponse } from 'next/server';
 
 export async function POST(request:any){
   const {userPhoto,userName,grade,subject,creationDate,photos,problemDescription,solutions,areWeFriends,problemDomain}=await request.json();
   await startDatabase();
-  await CommunityData.create({userPhoto,userName,grade,subject,creationDate,problemDescription,areWeFriends,problemDomain});
+  await CommunityDataSchema.create({userPhoto,userName,grade,subject,creationDate,photos,problemDescription,solutions,areWeFriends,problemDomain});
   return NextResponse.json({ message: "Post Created" }, { status: 201 });
-}
+  }
+
 
 
 export async function GET(){
-    await startDatabase();
-    const communityPostDataGet=await CommunityData.find();
-    return NextResponse.json({communityPostDataGet});
-  }
+  await startDatabase();
+  const denemeee=await CommunityDataSchema.find();
+  return NextResponse.json({ denemeee });
+}
